68169ffd056c440097f5aacf32d26bf6980f4f74
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-04-13 14:38:40 -0300

    Inicio

--- Frontend-Tesina/src/app/app.component.ts
+++ Frontend-Tesina/src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, InicioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Frontend-Tesina';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
+function initFlowbite() {
+  throw new Error('Function not implemented.');
 }
