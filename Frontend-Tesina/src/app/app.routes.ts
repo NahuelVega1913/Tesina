e4cfc79e180ed0b46a2a9e0710193b976c7bd477
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-04-13 19:14:09 -0300

    .

--- Frontend-Tesina/src/app/app.routes.ts
+++ Frontend-Tesina/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'inicio',
+    loadComponent: () =>
+      import('./inicio/inicio.component').then((m) => m.InicioComponent),
+  },
+  { path: 'registrarse', component: RegistrarseComponent },
+];
--- Frontend-Tesina/src/app/login/login.component.ts
+++ Frontend-Tesina/src/app/login/login.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  goToRegister() {
+    window.location.href = '/registrarse';
+  }
 }
