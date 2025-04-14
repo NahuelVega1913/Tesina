c12425e83673f40a712d0aab1775bcc6f508436f
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-04-14 00:01:10 -0300

    .

---
+++ Frontend-Tesina/src/app/repuesto/repuesto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repuesto',
+  imports: [],
+  templateUrl: './repuesto.component.html',
+  styleUrl: './repuesto.component.css'
+})
+export class RepuestoComponent {
+
+}
