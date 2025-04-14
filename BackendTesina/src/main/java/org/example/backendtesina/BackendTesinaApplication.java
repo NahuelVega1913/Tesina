9b4eacb1eb905a4efeb54ebdc2d0780bb43b4ddb
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-04-14 12:14:38 -0300

    Creacion del backend

---
+++ BackendTesina/src/main/java/org/example/backendtesina/BackendTesinaApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendtesina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTesinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTesinaApplication.class, args);
+	}
+
+}
---
+++ BackendTesina/src/test/java/org/example/backendtesina/BackendTesinaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.backendtesina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTesinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
