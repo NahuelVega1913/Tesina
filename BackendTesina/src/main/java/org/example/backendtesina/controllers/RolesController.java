4eeb6ab88ba7b5c0ff5ad4fbbdf95832980f0a4c
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-04-19 01:56:30 -0300

    fixes

--- BackendTesina/src/main/java/org/example/backendtesina/controllers/RolesController.java
+++ BackendTesina/src/main/java/org/example/backendtesina/controllers/RolesController.java
@@ -1,6 +1,8 @@
 package org.example.backendtesina.controllers;
 
+import org.example.backendtesina.entities.RoleEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +15,8 @@ public class RolesController {
 
     @GetMapping("/getAll")
     public int getAllRoles(){
+
         return 1;
     }
+    
 }
