d6874b10cc4316a78acc40f8ae05a9aadf53b612
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-04-23 18:05:20 -0300

    frontend repuestos

--- Frontend-Tesina/src/app/repuestos/repuestos.component.ts
+++ Frontend-Tesina/src/app/repuestos/repuestos.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-repuestos',
   imports: [],
   templateUrl: './repuestos.component.html',
-  styleUrl: './repuestos.component.css'
+  styleUrl: './repuestos.component.css',
 })
 export class RepuestosComponent {
+  constructor(private router: Router) {}
 
+  redirectTo(url: string) {
+    this.router.navigate([`${url}`]);
+  }
 }
