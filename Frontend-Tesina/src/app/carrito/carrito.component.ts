ec7e27f8973eb2266101aec734393255aa306f47
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-04-29 01:14:28 -0300

    fixes carrito

--- Frontend-Tesina/src/app/carrito/carrito.component.ts
+++ Frontend-Tesina/src/app/carrito/carrito.component.ts
@@ -2,10 +2,16 @@ import { Component, inject } from '@angular/core';
 import { CartService } from '../services/cart.service';
 import { map } from 'rxjs';
 import Swal from 'sweetalert2';
+import {
+  FormsModule,
+  UntypedFormArray,
+  UntypedFormControl,
+  UntypedFormGroup,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-carrito',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './carrito.component.html',
   styleUrl: './carrito.component.css',
 })
@@ -13,6 +19,9 @@ export class CarritoComponent {
   private service: CartService = inject(CartService);
   carrito: any[] = [];
   productos: any[] = [];
+  form = new UntypedFormGroup({
+    Cantidades: new UntypedFormArray([]),
+  });
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
@@ -29,6 +38,7 @@ export class CarritoComponent {
           id: spare.id,
           name: spare.name,
           price: spare.price,
+          cantidad: 1,
           discaunt: spare.discaunt,
           active: spare.active,
           image1: spare.image1,
@@ -52,8 +62,39 @@ export class CarritoComponent {
   calculateSubtotal() {
     let subtotal = 0;
     this.productos.forEach((producto) => {
-      subtotal += producto.price;
+      subtotal += producto.price * producto.cantidad;
     });
     return subtotal;
+  }
+  calculateDiscaunt() {
+    let total = 0;
+    this.productos.forEach((producto) => {
+      const descuento =
+        (producto.discaunt / 100) * producto.price * producto.cantidad;
+      const precioConDescuento = producto.price * producto.cantidad;
+      total += descuento;
+    });
+    return total;
+  }
+  calculateTotal() {
+    let total = 0;
+    this.productos.forEach((producto) => {
+      const descuento = (producto.discaunt / 100) * producto.price;
+      const precioConDescuento = producto.price - descuento;
+      total += precioConDescuento * producto.cantidad;
+    });
+    return total;
+  }
+  decrementarUnidad(index: number) {
+    if (this.productos[index].cantidad <= 1) {
+    } else {
+      this.productos[index].cantidad--;
+    }
+  }
+  aumentarUnidad(index: number) {
+    if (this.productos[index].cantidad >= 5) {
+    } else {
+      this.productos[index].cantidad++;
+    }
   }
 }
