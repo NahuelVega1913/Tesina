d1d14209abc37bc97dfce69befdbecc1a5dfdfe4
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-05-05 00:05:48 -0300

    monday

--- BackendTesina/src/main/java/org/example/backendtesina/controllers/NotificationController.java
+++ BackendTesina/src/main/java/org/example/backendtesina/controllers/NotificationController.java
@@ -1,6 +1,8 @@
 package org.example.backendtesina.controllers;
 
 import org.apache.coyote.Response;
+import org.example.backendtesina.DTOs.Get.GetCartDTO;
+import org.example.backendtesina.entities.NotificationEntity;
 import org.example.backendtesina.services.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,5 +21,25 @@ public class NotificationController {
     @PutMapping(value = "putNotification")
     public ResponseEntity<?> putNotification(@RequestHeader("Authorization") String authorizationHeader){
         return null;
+    }
+    @PostMapping(value = "createUser")
+    public ResponseEntity<?> createUserNotification(@RequestHeader("Authorization") String authorizationHeader){
+        String token = authorizationHeader.substring(7); // Elimina "Bearer "
+        NotificationEntity entity = service.createUserNotification(token);
+        if(entity == null){
+            return ResponseEntity.badRequest().build();
+        }else{
+            return ResponseEntity.ok("Notificacion creada");
+        }
+    }
+    @PostMapping(value = "buyProduct")
+    public ResponseEntity<?> buyProductNotification(@RequestHeader("Authorization") String authorizationHeader){
+        String token = authorizationHeader.substring(7); // Elimina "Bearer "
+        NotificationEntity entity = service.createUserNotification(token);
+        if(entity == null){
+            return ResponseEntity.badRequest().build();
+        }else{
+            return ResponseEntity.ok("Notificacion creada");
+        }
     }
 }
--- BackendTesina/src/main/java/org/example/backendtesina/services/NotificationService.java
+++ BackendTesina/src/main/java/org/example/backendtesina/services/NotificationService.java
@@ -1,7 +1,45 @@
 package org.example.backendtesina.services;
 
+import org.example.backendtesina.entities.NotificationEntity;
+import org.example.backendtesina.entities.UserEntity;
+import org.example.backendtesina.jwt.JwtService;
+import org.example.backendtesina.repositories.NotificationRepository;
+import org.example.backendtesina.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
+    @Autowired
+    JwtService jwtService;
+    @Autowired
+    NotificationRepository repository;
+    @Autowired
+    UserRepository userRepository;
+
+    public NotificationEntity createUserNotification(String token){
+        String email = jwtService.getEmailFromToken(token);
+        UserEntity user = userRepository.findByEmail(email).orElse(null);
+        return null;
+    }
+    public NotificationEntity purchasedProduct(String token){
+        String email = jwtService.getEmailFromToken(token);
+        UserEntity user = userRepository.findByEmail(email).orElse(null);
+        return null;
+    }
+    public NotificationEntity serviceFinalizedNotification(String token){
+        String email = jwtService.getEmailFromToken(token);
+        UserEntity user = userRepository.findByEmail(email).orElse(null);
+        return null;
+    }
+    public NotificationEntity productSell(String token){
+        String email = jwtService.getEmailFromToken(token);
+        UserEntity user = userRepository.findByEmail(email).orElse(null);
+        return null;
+    }
+    public NotificationEntity readNotifications(String token){
+        String email = jwtService.getEmailFromToken(token);
+        UserEntity user = userRepository.findByEmail(email).orElse(null);
+        return null;
+    }
 }
