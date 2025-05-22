4a45ff4e5db9cfcf6db86285181f636dd7c7e40f
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-05-21 22:34:48 -0300

    chat bot frontend

--- Frontend-Tesina/src/app/chatbot/chatbot.component.ts
+++ Frontend-Tesina/src/app/chatbot/chatbot.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-chatbot',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './chatbot.component.html',
-  styleUrl: './chatbot.component.css'
+  styleUrl: './chatbot.component.css',
 })
 export class ChatbotComponent {
+  mensajesBot: any[] = [
+    'Hola, como estas?',
+    'Excelente!',
+    'En que puedo ayudarte?',
+    'Que tal?',
+  ];
+  mensajesUsuario: any[] = [
+    'hola',
+    'bien y vos?',
+    'Tengo una consulta',
+    'que tal?',
+  ];
+  mensajeUsuario: string = '';
 
+  agregarMensajeUsuario() {
+    this.mensajesUsuario.push(this.mensajeUsuario);
+  }
+  agregarMensajeBot() {
+    this.mensajesUsuario.push('Mensaje de Bot');
+  }
 }
