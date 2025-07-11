deb45cb2bc013e045279d301d5d961f84f97b0b7
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-07-11 18:19:49 -0300

    fixes

--- Frontend-Tesina/src/app/terminos-condiciones/terminos-condiciones.component.ts
+++ Frontend-Tesina/src/app/terminos-condiciones/terminos-condiciones.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
-
+import { AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+declare const initFlowbite: any;
+declare var window: any;
+setTimeout(() => {
+  if (window.initFlowbite) {
+    window.initFlowbite();
+  }
+}, 0);
 @Component({
   selector: 'app-terminos-condiciones',
   imports: [],
   templateUrl: './terminos-condiciones.component.html',
-  styleUrl: './terminos-condiciones.component.css'
+  styleUrl: './terminos-condiciones.component.css',
 })
-export class TerminosCondicionesComponent {
+export class TerminosCondicionesComponent implements AfterViewInit {
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      setTimeout(() => initFlowbite(), 0); // Espera al render
+    });
+  }
 
+  ngAfterViewInit() {
+    initFlowbite();
+  }
 }
