881d38edd463c3c1bfc918eb65eb52c07d2fa373
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-07-11 12:57:34 -0300

    qqq

--- Frontend-Tesina/src/app/espera/espera.component.ts
+++ Frontend-Tesina/src/app/espera/espera.component.ts
@@ -66,6 +66,23 @@ export class EsperaComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     // No hagas nada acá si hay *ngIf
+    const map = L.map('map').setView([-31.4188, -64.2327], 15);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors',
+    }).addTo(map);
+
+    const icon = L.icon({
+      iconUrl: 'assets/leaflet/marker-icon.png',
+      iconRetinaUrl: 'assets/leaflet/marker-icon-2x.png',
+      shadowUrl: 'assets/leaflet/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41],
+    });
+
+    L.marker([-34.6, -58.4], { icon }).addTo(map);
   }
 
   ngAfterViewChecked(): void {
