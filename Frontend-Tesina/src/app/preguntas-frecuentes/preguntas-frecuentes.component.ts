71c8a9bc062e973402bfde6ef173d1b3cdeb5340
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-07-14 16:01:21 -0300

    preguntas frecuentes

--- Frontend-Tesina/src/app/preguntas-frecuentes/preguntas-frecuentes.component.ts
+++ Frontend-Tesina/src/app/preguntas-frecuentes/preguntas-frecuentes.component.ts
@@ -1,18 +1,29 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-preguntas-frecuentes',
-  imports: [],
+  imports: [NgClass, FormsModule],
   templateUrl: './preguntas-frecuentes.component.html',
   styleUrl: './preguntas-frecuentes.component.css',
 })
 export class PreguntasFrecuentesComponent {
   rol: any = '';
+  // Estados de los acordeones para USER y para otros roles
+  userOpen: boolean[] = [false, false, false, false];
+  otherOpen: boolean[] = [false, false, false, false];
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
     this.rol = localStorage.getItem('role');
     console.log(this.rol);
+  }
+
+  toggleAccordion(index: number, isUser: boolean) {
+    if (isUser) {
+      this.userOpen[index] = !this.userOpen[index];
+    } else {
+      this.otherOpen[index] = !this.otherOpen[index];
+    }
   }
 }
