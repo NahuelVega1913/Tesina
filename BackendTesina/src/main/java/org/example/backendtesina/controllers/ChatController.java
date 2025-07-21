308900efedc42acdb4c7f568148ecdcd8d164619
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-07-21 12:29:03 -0300

    retry

--- BackendTesina/src/main/java/org/example/backendtesina/controllers/ChatController.java
+++ BackendTesina/src/main/java/org/example/backendtesina/controllers/ChatController.java
@@ -2,7 +2,7 @@ package org.example.backendtesina.controllers;
 
 
 import org.example.backendtesina.DTOs.Get.GetEmployee;
-import org.example.backendtesina.services.ChatService;
+import org.example.backendtesina.services.ChatService2;
 import org.example.backendtesina.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +18,7 @@ import java.util.List;
 public class ChatController {
 
     @Autowired
-    ChatService service;
+    ChatService2 service;
 
     @PostMapping(value = "postmessage")
     public ResponseEntity<?> postMesagge(@RequestBody String message){
