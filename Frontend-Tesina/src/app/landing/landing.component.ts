ccea22d99ddf414e5212ed6518dd39c50c094ac3
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-07-31 18:42:12 -0300

    landing

--- Frontend-Tesina/src/app/landing/landing.component.ts
+++ Frontend-Tesina/src/app/landing/landing.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   imports: [],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styleUrl: './landing.component.css',
 })
 export class LandingComponent {
+  constructor(private router: Router) {}
 
+  navigate(route: string) {
+    // Implement navigation logic here
+    this.router.navigate([route]);
+  }
 }
