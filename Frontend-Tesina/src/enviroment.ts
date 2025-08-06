51ccdbf350b099e6ff1fc45b27a508e3c1111314
NahuelVega1913 <nahuelvegacsharp@gmail.com>
2025-08-05 23:34:47 -0300

    f

--- Frontend-Tesina/src/enviroment.ts
+++ Frontend-Tesina/src/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://tesis-backend-f1x5.onrender.com/',
-  apiUrl: 'http://localhost:8080/',
+  apiUrl: 'https://tesis-backend-f1x5.onrender.com/',
+  //apiUrl: 'http://localhost:8080/',
 };
